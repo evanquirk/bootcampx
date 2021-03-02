3b6a2bfe4c444d4b9309abd205db6b372d846052
Evan Quirk <evan_quirk@me.com>
2021-03-01 21:33:35 -0800

    chore: queries with group by/having

--- 3_queries_group-by/5_Average_completion_time.sql
+++ 3_queries_group-by/5_Average_completion_time.sql
@@ -1,7 +1,7 @@
 SELECT students.name AS student,
       avg(assignment_submissions.duration) AS average_assignment_duration
   FROM students
-  LEFT OUTER JOIN assignment_submissions ON student_id = students.id
+  JOIN assignment_submissions ON student_id = students.id
   JOIN assignments on assignment_id = assignments.id
   WHERE students.end_date IS NULL
   GROUP BY students.name
