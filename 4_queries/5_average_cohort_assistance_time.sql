29da5f4833cc931512546929625c8f9ba2ac8327
Evan Quirk <evan_quirk@me.com>
2021-03-02 17:50:30 -0800

    get the average duration of assistance requests for each cohort

--- 4_queries/5_average_cohort_assistance_time.sql
+++ 4_queries/5_average_cohort_assistance_time.sql
@@ -1,3 +1,5 @@
+-- Get the average duration of assistance requests for each cohort.
+
 SELECT cohorts.name, avg(completed_at - started_at) AS average_assistance_time
 FROM assistance_requests
 JOIN students ON students.id = student_id
