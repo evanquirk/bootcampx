a6754c93c3dfc485a376cf5f56b5ee323697b9ee
Evan Quirk <evan_quirk@me.com>
2021-03-02 17:49:06 -0800

    get total number of assistance requests for a teacher

--- 4_queries/1_total_teacher_assistance.sql
+++ 4_queries/1_total_teacher_assistance.sql
@@ -1,3 +1,5 @@
+-- Get the total number of assistance_requests for a teacher.
+
 SELECT count(*) AS total_assistances FROM teachers, name
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
